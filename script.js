f7ef70a7e7d5acd0b5af65cdedccb38fb61b5af7
aftab-alam02526 <aftab0447@gmail.com>
2025-10-04 12:00:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
 //your JS code here. If required.
+
+function delay(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+document.getElementById("btn").addEventListener("click", async function () {
+
+  const text = document.getElementById("text").value;
+  const delayTime = parseInt(document.getElementById("delay").value);
+
+ 
+  const outputDiv = document.getElementById("output");
+  outputDiv.textContent = "";
+
+  
+  await delay(delayTime);
+
+  
+  outputDiv.textContent = text;
+});
